c5fba45a7b5754d50d7517e528014f0a8af4f23d
Akash Kumar <akashkumar827182@gmail.com>
2024-06-26 23:26:57 +0530

    add to course added

--- server/index.js
+++ server/index.js
@@ -15,11 +15,17 @@ app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
+app.use("/uploads", express.static("uploads"));
+
 // importing routes
 import userRoutes from "./routes/user.js";
+import courseRoutes from "./routes/course.js";
+import adminRoutes from "./routes/admin.js";
 
 // using routes
 app.use("/api", userRoutes);
+app.use("/api", courseRoutes);
+app.use("/api", adminRoutes);
 
 app.listen(5000, () => {
   console.log(`Server is running on http://localhost:${port}`);
